337925115c3e6bbe21725f6bd286ac733aa5b50b
Michael Sartin-Tarm <msartintarm@gmail.com>
2019-04-27 21:44:07 +0000

    Adding 'about' section

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -16,11 +16,22 @@ class App extends Component {
             <button className="top-tab-button">
                 Game2
             </button>
+            <button className="top-tab-button">
+                About
+            </button>
           </header>
           <div className="content">
             <p>
               Hey there! This is an awesome view.
             </p>
+          </div>
+          <div className="content-spacer"></div>
+          <div class="content">
+            <h1>About</h1>
+            <h2>About site</h2>
+            <p>This site demonstrates several interactive Javascript games.</p>
+            <h2>About creator</h2>
+            <p>Michael Sartin-Tarm is from Wisconsin and enjoys music, games, and beer.</p>
           </div>
         </div>
         <Route path="/" component={IndexView} />
