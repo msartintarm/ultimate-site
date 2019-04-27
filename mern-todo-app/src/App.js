85001df5bcfc305242f0ce79681b22b4b39a237d
Michael Sartin-Tarm <msartintarm@gmail.com>
2019-04-27 22:14:18 +0000

    Added multiple content sections. Tab size 2.

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -10,32 +10,58 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <button className="top-tab-button">
-                Game1
-            </button>
-            <button className="top-tab-button">
-                Game2
-            </button>
-            <button className="top-tab-button">
-                About
-            </button>
+            <button className="top-tab-button">Game1</button>
+            <button className="top-tab-button">Game2</button>
+            <button className="top-tab-button">About</button>
           </header>
-          <div className="content">
-            <p>
-              Hey there! This is an awesome view.
-            </p>
-          </div>
+          <Game1Content/>
           <div className="content-spacer"></div>
-          <div class="content">
-            <h1>About</h1>
-            <h2>About site</h2>
-            <p>This site demonstrates several interactive Javascript games.</p>
-            <h2>About creator</h2>
-            <p>Michael Sartin-Tarm is from Wisconsin and enjoys music, games, and beer.</p>
-          </div>
+          <Game2Content/>
+          <div className="content-spacer"></div>
+          <AboutContent/>
         </div>
         <Route path="/" component={IndexView} />
       </Router>
+    );
+  }
+}
+
+class Game1Content extends Component {
+  render() {
+    return (
+      <div className="content">
+        <h1>Game 1</h1>
+        <p>
+          Hey there! This is an awesome view.
+        </p>
+      </div>
+    );
+  }
+}
+
+class Game2Content extends Component {
+  render() {
+    return (
+      <div class="content">
+        <h1>Game 2</h1>
+        <p>
+          Hey there! This is an awesome view.
+        </p>
+      </div>
+    );
+  }
+}
+
+class AboutContent extends Component {
+  render() {
+    return (
+      <div class="content">
+        <h1>About</h1>
+        <h2>About site</h2>
+        <p>This site demonstrates several interactive Javascript games.</p>
+        <h2>About creator</h2>
+        <p>Michael Sartin-Tarm is from Wisconsin and enjoys music, games, and beer.</p>
+      </div>
     );
   }
 }
