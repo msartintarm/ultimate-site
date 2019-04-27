bd710542f69933a6e01326484b1925d5388b50c8
Michael Sartin-Tarm <msartintarm@gmail.com>
2019-04-27 06:20:12 +0000

    Replace generated content with placeholder stuff

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -10,18 +10,15 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+            <button className="top-tab-buttton">
+                Game1
+            </button>
+            <button className="top-tab-button">
+                Game2
+            </button>
             <p>
-              Edit <code>src/App.js</code> and save to reload.
+              Hey there! This is an awesome view.
             </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
           </header>
         </div>
         <Route path="/" component={IndexView} />
